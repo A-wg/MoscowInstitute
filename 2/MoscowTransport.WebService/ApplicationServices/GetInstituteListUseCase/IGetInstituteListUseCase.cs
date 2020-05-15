727800f29599563c598db621949d9d2fe5048956
A-wg <65422387+A-wg@users.noreply.github.com>
2020-05-15 23:26:34 +0300

    Add files via upload

---
+++ 2/MoscowTransport.WebService/ApplicationServices/GetInstituteListUseCase/IGetInstituteListUseCase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MoscowInstitute.ApplicationServices.Interfaces;
+
+namespace MoscowInstitute.ApplicationServices.GetInstituteListUseCase
+{
+    public interface IGetInstituteListUseCase : IUseCase<GetInstituteListUseCaseRequest, GetInstituteListUseCaseResponse>
+    {
+    }
+}
