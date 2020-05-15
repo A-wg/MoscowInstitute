95f9fb5b0295254db88f0609adbe27c09a3c6646
A-wg <65422387+A-wg@users.noreply.github.com>
2020-05-15 23:28:25 +0300

    Add files via upload

---
+++ 1/MoscowTransport.WebService/Controllers/RoutesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MoscowInstitute.DomainObjects;
+
+namespace MoscowInstitute.WebService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RoutesController : ControllerBase
+    {
+        private readonly ILogger<RoutesController> _logger;
+
+        public RoutesController(ILogger<RoutesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Institute> GetRoute()
+        {
+            return new List<Institute>() 
+            { 
+                new Institute() 
+                {
+                 Id = 1,
+                    NameInstitute = "ГБОУ ДО ДТДМ «Хорошево»",
+                    Site = "dtim.mskobr.ru",
+                    Address = "123154, ГОРОД МОСКВА, УЛИЦА МАРШАЛА ТУХАЧЕВСКОГО, 20, 1",
+                    Program = "Дополнительное образование | дополнительное образование детей # Дополнительное образование | дополнительное профессиональное образование # Дополнительное образование | дополнительное образование взрослых  "
+
+                }
+            };
+        }
+    }
+}
---
+++ 1/MoscowTransport.WebService/DomainObjects/Ports/IInstituteRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+
+namespace MoscowInstitute.DomainObjects.Ports
+{
+    public interface IReadOnlyInstituteRepository
+    {
+        Task<Institute> GetInstitute(long id);
+
+        Task<IEnumerable<Institute>> GetAllInstitute();
+
+        Task<IEnumerable<Institute>> QueryInstitute(ICriteria<Institute> criteria);
+
+    }
+
+    public interface IInstituteRepository
+    {
+        Task AddInstitute(Institute institute);
+
+        Task RemoveInstitute(Institute institute);
+
+        Task UpdateInstitute(Institute institute);
+    }
+}
