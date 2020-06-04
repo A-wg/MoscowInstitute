9149b71d9d8c5eb6344d91c1bf10d8f0bbffe2dc
A-wg <65422387+A-wg@users.noreply.github.com>
2020-06-04 12:12:43 +0300

    Add files via upload

---
+++ 7lr/MoscowInstitute.DesktopClient/App.xaml.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MoscowInstitute.ApplicationServices.GetInstituteListUseCase;
+using MoscowInstitute.ApplicationServices.Ports.Cache;
+using MoscowInstitute.ApplicationServices.Repositories;
+using MoscowInstitute.DesktopClient.InfrastructureServices.ViewModels;
+using MoscowInstitute.DomainObjects;
+using MoscowInstitute.DomainObjects.Ports;
+using MoscowInstitute.InfrastructureServices.Cache;
+using MoscowInstitute.InfrastructureServices.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MoscowInstitute.DesktopClient 
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private readonly ServiceProvider _serviceProvider;
+
+        public App()
+        {
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+            _serviceProvider = serviceCollection.BuildServiceProvider();
+        }
+
+        private void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<IDomainObjectsCache<Institute>, DomainObjectsMemoryCache<Institute>>();
+            services.AddSingleton<NetworkInstituteRepository>(
+                x => new NetworkInstituteRepository("localhost", 80, useTls: false, x.GetRequiredService<IDomainObjectsCache<Institute>>())
+            );
+            services.AddSingleton<CachedReadOnlyInstituteRepository>(
+                x => new CachedReadOnlyInstituteRepository(
+                    x.GetRequiredService<NetworkInstituteRepository>(),
+                    x.GetRequiredService<IDomainObjectsCache<Institute>>()
+                )
+            );
+            services.AddSingleton<IReadOnlyInstituteRepository>(x => x.GetRequiredService<CachedReadOnlyInstituteRepository>());
+            services.AddSingleton<IGetInstituteListUseCase, GetInstituteListUseCase>();
+            services.AddSingleton<MainViewModel>();
+            services.AddSingleton<MainWindow>();
+        }
+
+        private void OnStartup(object sender, StartupEventArgs args)
+        {
+            var mainWindow = _serviceProvider.GetService<MainWindow>();
+            mainWindow.Show();
+        }
+    }
+}
---
+++ 7lr/MoscowInstitute.DesktopClient/InfrastructureServices/Repositories/NetworkInstituteRepository.cs
@@ -0,0 +1,46 @@
+﻿using MoscowInstitute.ApplicationServices.Ports.Cache;
+using MoscowInstitute.DomainObjects;
+using MoscowInstitute.DomainObjects.Ports;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace MoscowInstitute.InfrastructureServices.Repositories
+{
+    public class NetworkInstituteRepository : NetworkRepositoryBase, IReadOnlyInstituteRepository
+    {
+        private readonly IDomainObjectsCache<Institute> _instituteCache;
+
+        public NetworkInstituteRepository(string host, ushort port, bool useTls, IDomainObjectsCache<Institute> instituteCache)
+            : base(host, port, useTls)
+            => _instituteCache = instituteCache;
+
+        public async Task<Institute> GetInstitute(long id)
+            => CacheAndReturn(await ExecuteHttpRequest<Institute>($"Institute/{id}"));
+
+        public async Task<IEnumerable<Institute>> GetAllInstitutes()
+            => CacheAndReturn(await ExecuteHttpRequest<IEnumerable<Institute>>($"Institute"), allObjects: true);
+
+        public async Task<IEnumerable<Institute>> QueryInstitutes(ICriteria<Institute> criteria)
+            => CacheAndReturn(await ExecuteHttpRequest<IEnumerable<Institute>>($"Institute"), allObjects: true)
+               .Where(criteria.Filter.Compile());
+
+        private IEnumerable<Institute> CacheAndReturn(IEnumerable<Institute> institutes, bool allObjects = false)
+        {
+            if (allObjects)
+            {
+                _instituteCache.ClearCache();
+            }
+            _instituteCache.UpdateObjects(institutes, DateTime.Now.AddDays(1), allObjects);
+            return institutes;
+        }
+
+        private Institute CacheAndReturn(Institute institute)
+        {
+            _instituteCache.UpdateObject(institute, DateTime.Now.AddDays(1));
+            return institute;
+        }
+    }
+}
