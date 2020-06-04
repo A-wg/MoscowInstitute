491328e19127a6bc5efb7c93d9298ea6a589490e
A-wg <65422387+A-wg@users.noreply.github.com>
2020-06-04 11:30:14 +0300

    Add files via upload

---
+++ 6lr/MoscowInstitutes.DesktopClient/App.xaml.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MoscowInstitute.ApplicationServices.GetMInstituteListUseCase;
+using MoscowInstitute.ApplicationServices.Ports.Cache;
+using MoscowInstitute.ApplicationServices.Repositories;
+using MoscowInstitute.DesktopClient.InfrastructureServices.ViewModels;
+using MoscowInstitute.DomainObjects;
+using MoscowInstitute.DomainObjects.Ports;
+using MoscowInstitute.InfrastructureServices.Cache;
+using MoscowInstitute.InfrastructureServices.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MoscowInstitute.DesktopClient
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private readonly ServiceProvider _serviceProvider;
+
+        public App()
+        {
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+            _serviceProvider = serviceCollection.BuildServiceProvider();
+        }
+
+        private void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<IDomainObjectsCache<DomainObjects.MoscowInstitute>, DomainObjectsMemoryCache<DomainObjects.MoscowInstitute>>();
+            services.AddSingleton<NetworkInstituteRepository>(
+                x => new NetworkInstituteRepository("localhost", 80, useTls: false, x.GetRequiredService<IDomainObjectsCache<DomainObjects.MoscowInstitute>>())
+            );
+            services.AddSingleton<CachedReadOnlyInstituteRepository>(
+                x => new CachedReadOnlyInstituteRepository(
+                    x.GetRequiredService<NetworkInstituteRepository>(), 
+                    x.GetRequiredService<IDomainObjectsCache<DomainObjects.MoscowInstitute>>()
+                )
+            );
+            services.AddSingleton<IReadOnlyInstituteRepository>(x => x.GetRequiredService<CachedReadOnlyInstituteRepository>());
+            services.AddSingleton<IGetMInstituteListUseCase, GetMInstituteListUseCase>();
+            services.AddSingleton<MainViewModel>();
+            services.AddSingleton<MainWindow>();
+        }
+
+        private void OnStartup(object sender, StartupEventArgs args)
+        {
+            var mainWindow = _serviceProvider.GetService<MainWindow>();
+            mainWindow.Show();
+        }
+    }
+}
---
+++ 6lr/MoscowInstitutes.DesktopClient/InfrastructureServices/Views/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using MoscowInstitute.DesktopClient.InfrastructureServices.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+using Newtonsoft.Json;
+using MoscowInstitute.InfrastructureServices.Gateways.Database;
+using MoscowInstitutes.WebService.InfrastructureServices.Gateways;
+using Microsoft.EntityFrameworkCore;
+using System.IO;
+using Microsoft.Data.Sqlite;
+
+namespace MoscowInstitute.DesktopClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow(MainViewModel viewModel)
+        {
+            InitializeComponent();
+            DataContext = viewModel;
+        }
+
+    }
+}
