dab3b325f3c74902266916b548b5c62bc806e16c
A-wg <65422387+A-wg@users.noreply.github.com>
2020-06-04 13:16:18 +0300

    Add files via upload

---
+++ 2pr/MoscowInstitute.DesktopClient/InfrastructureServices/Repositories/CachedReadOnlyMInstituteRepository.cs
@@ -0,0 +1,33 @@
+﻿using MoscowInstitute.ApplicationServices.Ports.Cache;
+using MoscowInstitute.DomainObjects;
+using MoscowInstitute.DomainObjects.Ports;
+using MoscowInstitute.DomainObjects.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoscowInstitute.InfrastructureServices.Repositories
+{
+    public class CachedReadOnlyMInstituteRepository : ReadOnlyMInstituteRepositoryDecorator
+    {
+        private readonly IDomainObjectsCache<DomainObjects.MoscowInstitutes> _routesCache;
+
+        public CachedReadOnlyMInstituteRepository(IReadOnlyInstituteRepository routeRepository, 
+                                             IDomainObjectsCache<DomainObjects.MoscowInstitutes> routesCache)
+            : base(routeRepository)
+            => _routesCache = routesCache;
+
+        public async override Task<DomainObjects.MoscowInstitutes> GetInstitute(long id)
+            => _routesCache.GetObject(id) ?? await base.GetInstitute(id);
+
+        public async override Task<IEnumerable<DomainObjects.MoscowInstitutes>> GetAllInstitutes()
+            => _routesCache.GetObjects() ?? await base.GetAllInstitutes();
+
+        public async override Task<IEnumerable<DomainObjects.MoscowInstitutes>> QueryInstitutes(ICriteria<DomainObjects.MoscowInstitutes> criteria)
+            => _routesCache.GetObjects()?.Where(criteria.Filter.Compile()) ?? await base.QueryInstitutes(criteria);
+
+    }
+}
---
+++ 2pr/MoscowInstitute.DesktopClient/InfrastructureServices/Repositories/NetworkInstituteRepository.cs
@@ -0,0 +1,46 @@
+﻿using MoscowInstitute.ApplicationServices.Ports.Cache;
+using MoscowInstitute.DomainObjects;
+using MoscowInstitute.DomainObjects.Ports;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace MoscowInstitute.InfrastructureServices.Repositories
+{
+    public class NetworkInstituteRepository : NetworkRepositoryBase, IReadOnlyInstituteRepository
+    {
+        private readonly IDomainObjectsCache<DomainObjects.MoscowInstitutes> _instituteCache;
+
+        public NetworkInstituteRepository(string host, ushort port, bool useTls, IDomainObjectsCache<DomainObjects.MoscowInstitutes> instituteCache)
+            : base(host, port, useTls)
+            => _instituteCache = instituteCache;
+
+        public async Task<DomainObjects.MoscowInstitutes> GetInstitute(long id)
+            => CacheAndReturn(await ExecuteHttpRequest<DomainObjects.MoscowInstitutes>($"MInstitute/{id}"));
+
+        public async Task<IEnumerable<DomainObjects.MoscowInstitutes>> GetAllInstitutes()
+            => CacheAndReturn(await ExecuteHttpRequest<IEnumerable<DomainObjects.MoscowInstitutes>>($"MInstitute"), allObjects: true);
+
+        public async Task<IEnumerable<DomainObjects.MoscowInstitutes>> QueryInstitutes(ICriteria<DomainObjects.MoscowInstitutes> criteria)
+            => CacheAndReturn(await ExecuteHttpRequest<IEnumerable<DomainObjects.MoscowInstitutes>>($"MInstitute"), allObjects: true)
+               .Where(criteria.Filter.Compile());
+
+        private IEnumerable<DomainObjects.MoscowInstitutes> CacheAndReturn(IEnumerable<DomainObjects.MoscowInstitutes> routes, bool allObjects = false)
+        {
+            if (allObjects)
+            {
+                _instituteCache.ClearCache();
+            }
+            _instituteCache.UpdateObjects(routes, DateTime.Now.AddDays(1), allObjects);
+            return routes;
+        }
+
+        private DomainObjects.MoscowInstitutes CacheAndReturn(DomainObjects.MoscowInstitutes route)
+        {
+            _instituteCache.UpdateObject(route, DateTime.Now.AddDays(1));
+            return route;
+        }
+    }
+}
