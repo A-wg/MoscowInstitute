c7504dbb841846434b1c731414fa7a82b37de9d9
A-wg <65422387+A-wg@users.noreply.github.com>
2020-06-04 12:36:41 +0300

    Add files via upload

---
+++ 8lr/MoscowInstitute.DesktopClient/ApplicationServices/Repositories/CachedReadOnlyInstituteRepository.cs
@@ -0,0 +1,32 @@
+﻿using MoscowInstitute.ApplicationServices.Ports.Cache;
+using MoscowInstitute.DomainObjects;
+using MoscowInstitute.DomainObjects.Ports;
+using MoscowInstitute.DomainObjects.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoscowInstitute.InfrastructureServices.Repositories
+{
+    public class CachedReadOnlyInstituteRepository : ReadOnlyInstituteRepositoryDecorator
+    {
+        private readonly IDomainObjectsCache<Institute> _institutesCache;
+
+        public CachedReadOnlyInstituteRepository(IReadOnlyInstituteRepository instituteRepository,
+                                             IDomainObjectsCache<Institute> institutesCache)
+            : base(instituteRepository)
+            => _institutesCache = institutesCache;
+
+        public async override Task<Institute> GetInstitute(long id)
+            => _institutesCache.GetObject(id) ?? await base.GetInstitute(id);
+
+        public async override Task<IEnumerable<Institute>> GetAllInstitutes()
+            => _institutesCache.GetObjects() ?? await base.GetAllInstitutes();
+
+        public async override Task<IEnumerable<Institute>> QueryInstitutes(ICriteria<Institute> criteria)
+            => _institutesCache.GetObjects()?.Where(criteria.Filter.Compile()) ?? await base.QueryInstitutes(criteria);
+    }
+}
---
+++ 8lr/MoscowInstitute.DesktopClient/InfrastructureServices/ViewModels/MainViewModel.cs
@@ -0,0 +1,87 @@
+﻿using MoscowInstitute.ApplicationServices.GetInstituteListUseCase;
+using MoscowInstitute.ApplicationServices.Ports;
+using MoscowInstitute.DomainObjects;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Threading.Tasks;
+
+namespace MoscowInstitute.DesktopClient.InfrastructureServices.ViewModels 
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private readonly IGetInstituteListUseCase _getInstituteListUseCase;
+
+        public MainViewModel(IGetInstituteListUseCase getInstituteListUseCase)
+            => _getInstituteListUseCase = getInstituteListUseCase;
+
+        private Task<bool> _loadingTask;
+        private Institute _currentInstitute;
+        private ObservableCollection<Institute> _institutes;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Institute CurrentInstitute
+        {
+            get => _currentInstitute;
+            set
+            {
+                if (_currentInstitute != value)
+                {
+                    _currentInstitute = value;
+                    OnPropertyChanged(nameof(CurrentInstitute));
+                }
+            }
+        }
+
+        private async Task<bool> LoadInstitutes()
+        {
+            var outputPort = new OutputPort();
+            bool result = await _getInstituteListUseCase.Handle(GetInstituteListUseCaseRequest.CreateAllInstitutesRequest(), outputPort);
+            if (result)
+            {
+                Institutes = new ObservableCollection<Institute>(outputPort.Institutes);
+            }
+            return result;
+        }
+
+        public ObservableCollection<Institute> Institutes
+        {
+            get
+            {
+                if (_loadingTask == null)
+                {
+                    _loadingTask = LoadInstitutes();
+                }
+
+                return _institutes;
+            }
+            set
+            {
+                if (_institutes != value)
+                {
+                    _institutes = value;
+                    OnPropertyChanged(nameof(Institutes));
+                }
+            }
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private class OutputPort : IOutputPort<GetInstituteListUseCaseResponse>
+        {
+            public IEnumerable<Institute> Institutes { get; private set; }
+
+            public void Handle(GetInstituteListUseCaseResponse response)
+            {
+                if (response.Success)
+                {
+                    Institutes = new ObservableCollection<Institute>(response.Institutes);
+                }
+            }
+        }
+    }
+}
