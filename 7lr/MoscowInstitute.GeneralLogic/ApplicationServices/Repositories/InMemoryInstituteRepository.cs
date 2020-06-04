1350d4d7076afc4de19e342bb133a333e0728f1a
A-wg <65422387+A-wg@users.noreply.github.com>
2020-06-04 11:40:21 +0300

    Add files via upload

---
+++ 7lr/MoscowInstitute.GeneralLogic/ApplicationServices/Repositories/InMemoryInstituteRepository.cs
@@ -0,0 +1,72 @@
+﻿using MoscowInstitute.DomainObjects;
+using MoscowInstitute.DomainObjects.Ports;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoscowInstitute.ApplicationServices.Repositories
+{
+    public class InMemoryInstituteRepository : IReadOnlyInstituteRepository,
+                                           IInstituteRepository
+    {
+        private readonly List<Institute> _institutes = new List<Institute>();
+
+        public InMemoryInstituteRepository(IEnumerable<Institute> institutes = null)
+        {
+            if (institutes != null)
+            {
+                _institutes.AddRange(institutes);
+            }
+        }
+
+        public Task AddInstitute(Institute institute)
+        {
+            _institutes.Add(institute);
+            return Task.CompletedTask;
+        }
+
+        public Task<IEnumerable<Institute>> GetAllInstitutes()
+        {
+            return Task.FromResult(_institutes.AsEnumerable());
+        }
+
+        public Task<Institute> GetInstitute(long id)
+        {
+            return Task.FromResult(_institutes.Where(o => o.Id == id).FirstOrDefault());
+        }
+
+        public Task<IEnumerable<Institute>> QueryInstitutes(ICriteria<Institute> criteria)
+        {
+            return Task.FromResult(_institutes.Where(criteria.Filter.Compile()).AsEnumerable());
+        }
+
+        public Task RemoveInstitute(Institute institute)
+        {
+            _institutes.Remove(institute);
+            return Task.CompletedTask;
+        }
+
+        public Task UpdateInstitute(Institute institute)
+        {
+            var foundInstitute = GetInstitute(institute.Id).Result;
+            if (foundInstitute == null)
+            {
+                AddInstitute(institute);
+            }
+            else
+            {
+                if (foundInstitute != institute)
+                {
+                    _institutes.Remove(foundInstitute);
+                    _institutes.Add(institute);
+                }
+            }
+            return Task.CompletedTask;
+        }
+        public Task ParseAndPush()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
